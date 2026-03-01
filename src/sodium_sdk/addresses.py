59ca7486165a05af9d363c77db8a636b7fcbc7b6
Alex Cupra <alexcupra1123@gmail.com>
2026-03-01 20:56:00 +0000

    feat: add payment address derivation

--- src/sodium_sdk/addresses.py
+++ src/sodium_sdk/addresses.py
@@ -1,8 +1,20 @@
 from solders.pubkey import Pubkey
 from .networks import DEFAULT_NETWORK, get_program_id
-from .types import Network
+from .normalize import normalize_username
+from .types import Network, Platform
 
 def get_config_address(network: Network = DEFAULT_NETWORK) -> Pubkey:
     program_id = get_program_id(network)
     pda, _ = Pubkey.find_program_address([b"config"], program_id)
+    return pda
+
+def get_payment_address(
+    platform: Platform,
+    username: str,
+    network: Network = DEFAULT_NETWORK,
+) -> Pubkey:
+    norm = normalize_username(username)
+    program_id = get_program_id(network)
+    seeds = [b"escrow", platform.lower().encode(), norm.encode()]
+    pda, _ = Pubkey.find_program_address(seeds, program_id)
     return pda
