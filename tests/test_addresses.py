e227f5def3eb650be3747eebfde86d337a1c9b4f
Alex Cupra <alexcupra1123@gmail.com>
2026-03-02 00:38:12 +0000

    test: add identity address vectors

--- tests/test_addresses.py
+++ tests/test_addresses.py
@@ -1,4 +1,8 @@
-from sodium_sdk.addresses import get_config_address, get_payment_address
+from sodium_sdk.addresses import (
+    get_config_address,
+    get_payment_address,
+    get_identity_address,
+)
 
 def test_config_address():
     assert str(get_config_address("mainnet")) == "3WYJaf1ocPSbQAMjFhka5t8iusdH11Atw6FcxeuVYsTw"
@@ -13,3 +17,9 @@ def test_payment_alias():
 
 def test_payment_unicode():
     assert str(get_payment_address("twitter", "Ansem 🐂🀄️")) == "k7JEBhVHHWLrNdhJ87cPbpgJr98kDVQuqUb5HsVKW7S"
+
+def test_identity_blknoiz06():
+    assert str(get_identity_address("twitter", "@blknoiz06")) == "T4gKVCxkB3c6pERaS67fLBgMyg1FpGmjeNcrGvbuLY1"
+
+def test_identity_unicode():
+    assert str(get_identity_address("twitter", "Ansem 🐂🀄️")) == "83McthMovBoPHgXPVGsif61zkCZNEE7tmypdNkxh1cv5"
