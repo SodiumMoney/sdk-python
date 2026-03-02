30d60939ab29fd6dd06749884f456aa118501cdf
Alex Cupra <alexcupra1123@gmail.com>
2026-03-02 03:25:41 +0000

    feat: export public API

--- src/sodium_sdk/__init__.py
+++ src/sodium_sdk/__init__.py
@@ -1,1 +1,30 @@
-"""Sodium address SDK for Python."""
+from .normalize import normalize_username
+from .networks import NETWORKS, DEFAULT_NETWORK, get_program_id, set_mainnet_program_id
+from .addresses import (
+    get_config_address,
+    get_payment_address,
+    get_identity_address,
+    get_used_proof_address,
+    get_link_request_address,
+    get_telegram_link_request_address,
+)
+from .evm import get_evm_salt
+from .types import Platform, Network, SUPPORTED_PLATFORMS
+
+__all__ = [
+    "normalize_username",
+    "NETWORKS",
+    "DEFAULT_NETWORK",
+    "get_program_id",
+    "set_mainnet_program_id",
+    "get_config_address",
+    "get_payment_address",
+    "get_identity_address",
+    "get_used_proof_address",
+    "get_link_request_address",
+    "get_telegram_link_request_address",
+    "get_evm_salt",
+    "Platform",
+    "Network",
+    "SUPPORTED_PLATFORMS",
+]
